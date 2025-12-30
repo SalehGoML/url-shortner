cfaca4a71f072b12efe1f22819acbea2b4accbe1
SalehAskarzadeh <msaskarzadeh@gmail.com>
2025-12-30 21:21:13 +0330

    add AuthService with GORM implementation for AuthService

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/SalehGoML/internal/models"
 	"github.com/SalehGoML/internal/repository"
+	"github.com/SalehGoML/internal/utils"
 )
 
 type AuthService interface {
@@ -20,4 +23,43 @@ func NewAuthService(userRepo repository.UserRepository, jwtSecret string) AuthSe
 		userRepo:  userRepo,
 		jwtSecret: jwtSecret,
 	}
+}
+
+func (s *authService) Register(name, email, password string) (*models.User, error) {
+	hashedPassword, err := utils.HashPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &models.User{
+		Name:     name,
+		Email:    email,
+		Password: hashedPassword,
+	}
+
+	err = s.userRepo.Create(user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s *authService) Login(email, password string) (string, error) {
+	user, err := s.userRepo.GetByEmail(email)
+	if err != nil {
+		return "", errors.New("invalid email or password")
+	}
+
+	err = utils.CheckPassword(password, user.Password)
+	if err != nil {
+		return "", errors.New("invalid email or password")
+	}
+
+	token, err := utils.GenerateJWT(user.ID, s.jwtSecret)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
 }
